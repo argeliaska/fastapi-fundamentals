31c8982ee79ed17fd8a06e280ebb66f0150dcca8
Argelia Skakievich <argeliaska@gmail.com>
2022-08-13 11:08:33 -0500

    feat: Module 4: Serving data

--- carsharing.py
+++ carsharing.py
@@ -1,8 +1,45 @@
 from fastapi import FastAPI
+from fastapi import HTTPException
+from http import HTTPStatus
+from typing import Optional, List, Dict
+import uvicorn
 
 app = FastAPI()
 
-@app.get('/')
-def welcome():
-    """Return a friendly welcome message."""
-    return {'message':'Welcome to the Car Sharing service!'}
+db = [
+    {"id": 1, "size": "s", "fuel": "gasoline", "doors": 3, "transmission": "auto"},
+    {"id": 2, "size": "s", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 3, "size": "s", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 4, "size": "m", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 5, "size": "m", "fuel": "hybrid", "doors": 5, "transmission": "auto"},
+    {"id": 6, "size": "m", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 7, "size": "l", "fuel": "diesel", "doors": 5, "transmission": "manual"},
+    {"id": 8, "size": "l", "fuel": "electric", "doors": 5, "transmission": "auto"},
+    {"id": 9, "size": "l", "fuel": "hybrid", "doors": 5, "transmission": "auto"}
+]
+
+@app.get('/api/cars')
+# def get_cars(size:str|None=None, doors:int|None=None) -> list:
+def get_cars(size:Optional[str]=None, doors:Optional[int]=None) -> List:
+    """Return the list of cars"""
+    result = db
+    if size:
+        result = [car for car in result if car['size'] == size]
+    if doors:
+        result = [car for car in result if car['doors'] == doors]
+    return result
+
+@app.get('/api/cars/{id}')
+def get_car_by_id(id:int) -> Dict:
+    """Return the car that matches with the id"""
+    result = [car for car in db if car['id'] == id]
+    if result: 
+        return result[0]
+    else:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail=f'No car with id = {id}'
+        )
+
+if __name__=='__main__':
+    uvicorn.run('carsharing:app', reload=True)    
